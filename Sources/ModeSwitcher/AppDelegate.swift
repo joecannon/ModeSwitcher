f6ba104438f1a7f34dc40e725156ac634fc2075c
Joe <joe.house432@gmail.com>
2025-03-24 21:42:16 +0000

    feat: add AutoLaunch functionality to create and load launch agent plist

--- Sources/ModeSwitcher/AppDelegate.swift
+++ Sources/ModeSwitcher/AppDelegate.swift
@@ -4,9 +4,11 @@ import Foundation
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     let appStatusBar = StatusBarManager()
-    
+    let AutoLaunchInstance = AutoLaunch()
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         appStatusBar.updateStatusBarIcon()
+        AutoLaunchInstance.createAutoLaunchPlist()
     }
 
 }
