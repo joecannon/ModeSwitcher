eba189480b21332bf19debc839dc529741988d75
Joe <joe.house432@gmail.com>
2025-04-28 12:02:32 +0100

    refactor: remove AutoLaunch class and related functionality from AppDelegate

--- Sources/ModeSwitcher/AppDelegate.swift
+++ Sources/ModeSwitcher/AppDelegate.swift
@@ -2,13 +2,9 @@ import AppKit
 import Foundation
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-    
+
     let appStatusBar = StatusBarManager()
-    let AutoLaunchInstance = AutoLaunch()
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        appStatusBar.updateStatusBarIcon()
-        AutoLaunchInstance.createAutoLaunchPlist()
+        appStatusBar.updateStatusBarIcon() // Update the status bar icon
     }
-
 }
